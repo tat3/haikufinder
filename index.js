aa55cfc1dc5af2eb7e2fabbe5c1f8dab1a9efb2d
tat3 <tat.github@gmail.com>
2018-11-19 22:50:04 +0900

    Expressで公開

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const ashitaka = require('./lib/ashitaka.js')
+// const ashitaka = require('./lib/ashitaka.js')
+// ashitaka(10)
+const express = require('express')
+const HaikuFinder = require('./lib/haikufinder')
 
-ashitaka(10)
+const app = express()
+const server = app.listen(3000, () => {
+  console.log(`Node.js is listening to PORT: ${server.address().port}`)
+})
+
+const haikuFinder = new HaikuFinder()
+
+app.get('/haiku/:sentence', async (req, res, next) => {
+  const result = await haikuFinder.interpret(req.params.sentence)
+  res.send(result)
+})
