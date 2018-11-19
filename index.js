0be3a5082315f5f83218b54187e3cfbd1e3715a3
tat3 <tat.github@gmail.com>
2018-11-19 23:22:10 +0900

    heroku用にport番号を修正

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const HaikuFinder = require('./lib/haikufinder')
 
 const app = express()
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Node.js is listening to PORT: ${server.address().port}`)
 })
 
