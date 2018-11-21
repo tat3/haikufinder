047112a8202e5b56b03842d8e1f40ea7c3489006
tat3 <tat.github@gmail.com>
2018-11-21 20:24:40 +0900

    apiをpostリクエストに対応。ホームページを追加

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 // const ashitaka = require('./lib/ashitaka.js')
 // ashitaka(10)
 const express = require('express')
+const bodyParser = require('body-parser')
 const HaikuFinder = require('./lib/haikufinder')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Node.js is listening to PORT: ${server.address().port}`)
 })
 
 const haikuFinder = new HaikuFinder()
+
+app.get('/', async (req, res, next) => {
+  res.send(`
+<h1>Let's try your cool sentence!</h1>
+<form action="/" method="post">
+  <input name="sentence">
+  <button>send</button>
+</form>
+`)
+})
+
+app.post('/', async (req, res, next) => {
+  const sentence = req.body.sentence
+  if (sentence === undefined) {
+    return res.send([])
+  }
+  const result = await haikuFinder.interpret(sentence)
+  res.send(result)
+})
 
 app.get('/:sentence', async (req, res, next) => {
   const result = await haikuFinder.interpret(req.params.sentence)
